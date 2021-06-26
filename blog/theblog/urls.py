90a6049af30c48f84518202e5957d6e4640665ad
KaraPillay <82381695+KaraPillay@users.noreply.github.com>
2021-06-26 17:53:15 +0530

    Initial commit

---
+++ blog/theblog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+from .views import HomeView, ArticleDetailView
+urlpatterns = [
+    path('',HomeView.as_view(),name = "home"),
+    path('article/<int:pk>',ArticleDetailView.as_view() , name='post_detail'),
+    #path('', views.home, name="home")
+]
